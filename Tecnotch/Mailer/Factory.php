8352f0a8d53c7fbee86a5547ec20ac59a5f4afe1
Tofeeq ur Rehman <developer.tofeeq@gmail.com>
2016-04-22 14:36:05 +0500

    Removed unwanted debug info

--- Tecnotch/Mailer/Factory.php
+++ Tecnotch/Mailer/Factory.php
@@ -7,12 +7,10 @@ class Factory {
 	{
 		switch (\Tecnotch\Config::SMTP_MAILER) {
 			case 'phpmailer':
-				echo "returning phpmailer"; die;
 				return new Smtp\PhpMailer();
 				break;
 			
 			default:
-				echo "returning default swiftmailer"; die;
 				return new Smtp\Swift();
 				break;
 		}

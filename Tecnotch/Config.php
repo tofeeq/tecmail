3c887c4abc3aef6c29894cf03185a18477c35b61
tofeeq <tofeeq3@gmail.com>
2016-06-03 16:11:12 +0500

    Update Config.php

--- Tecnotch/Config.php
+++ Tecnotch/Config.php
@@ -3,14 +3,14 @@ namespace Tecnotch;
 
 class Config {
 
-	const SMTP_HOST = 'smtp.gmail.com';
-	const SMTP_PORT = 465;
+	const SMTP_HOST = 'smtp.gmail.com'; //provide your smtp host here 
+	const SMTP_PORT = 465;		//provide smtp port
 	//const SMTP_PORT = 587;
 
-	const SMTP_USER = 'your gmail id';
- 	const SMTP_PASS = 'your gmail password';
+	const SMTP_USER = 'your gmail id'; //provide your smpt username
+ 	const SMTP_PASS = 'your gmail password'; //provide your smtp password
 
- 	const SMTP_MAILER = 'swiftmailer';
+ 	const SMTP_MAILER = 'swiftmailer';  //configure smtp backend 
 
  	//or you can use phpmailer as below
  	//const SMTP_MAILER = 'phpmailer';
